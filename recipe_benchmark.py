fd07aa3bb986381e54985e4ea11092bc6b30e073
Tom White <tom.e.white@gmail.com>
2019-09-27 12:39:41 +0100

    Update run information

--- recipe_benchmark.py
+++ recipe_benchmark.py
@@ -1,11 +1,11 @@
 import time
-from scipy.sparse import random
+from scipy.sparse import issparse, random
 import numpy as np
 import scipy.sparse
 
 import dask.array as da
 import dask.array.random
-from scanpy.array import sparse_dask
+from scanpy.array import sparse_dask, SparseArray
 
 np.random.seed(42)
 
@@ -16,6 +16,7 @@ def filter_genes(X, min_number, sparse=False):
     gene_subset = number_per_gene >= min_number
     s = np.sum(~gene_subset)
     print("Filtered out", s)
+    print("gene_subset is a ", type(gene_subset))
     Y = X[:,gene_subset]
     return Y, number_per_gene # note we are returning "side data"
 
@@ -24,7 +25,9 @@ def log1p(X):
     return np.log1p(X)
 
 def densify(X):
-    return X.toarray()
+    if issparse(X):
+        return X.toarray()
+    return np.asarray(X)
 
 def scale(X):
     mean, var = _get_mean_var(X)
@@ -86,7 +89,6 @@ def time_sparse():
     Y = log1p(X)
     Y = densify(Y)
     Y = scale(Y)
-    Y = Y.sum() # call sum so we don't have to allocate output
     t2 = time.time()
     print("time to call filter_genes: ", t2-t1)
 
@@ -100,22 +102,58 @@ def time_sparse_dask():
     t1 = time.time()
     print("time to create matrix: ", t1-t0)
 
-    #Y, number_per_gene = filter_genes(X)
-    Y = log1p(X)
-    #Y = scale(Y)
-    Y = Y.sum() # call sum so we don't have to allocate output
-    da.compute(Y)
+    Y, number_per_gene = filter_genes(X, 5000)
+    Y = log1p(Y)
+    Y = Y.map_blocks(densify)
+    Y = scale(Y)
+    da.compute(Y, number_per_gene)
+    t2 = time.time()
+    print("time to call filter_genes: ", t2-t1)
+
+def time_pydata_sparse_dask():
+    print("time_pydata_sparse_dask")
+
+    import sparse
+
+    t0 = time.time()
+    X = dask.array.random.random((100000, 3000), chunks=(10000, 3000))
+    X[X < 0.90] = 0
+    X = X.map_blocks(sparse.COO)
+    X.compute()
+    #X = sparse.random((100000, 3000), 0.10) # gene expression matrix from 10x is 7% sparse, so this is similar
+    #X = da.from_array(X, chunks=(10000, X.shape[1]))
+    t1 = time.time()
+    print("time to create matrix: ", t1-t0)
+
+    Y, number_per_gene = filter_genes(X, 5000)
+    da.compute(Y, number_per_gene)
     t2 = time.time()
     print("time to call filter_genes: ", t2-t1)
 
 if __name__ == '__main__':
-    #time_numpy()
-
-    # For seeing how long tasks take
+    # Comment out to see how long tasks take
     #from dask.distributed import Client
     #client = Client(processes=False)
 
+    # Compare dense matrices
+    # time_numpy: 5.4s to create matrix, 10s to run recipe
+    # time_dask: 8.4s to create matrix, 4.6s to run recipe
+    # So we see that Dask can take advantage of cores to do the processing faster.
+
+    #time_numpy()
+
     #time_dask()
 
+    # Compare sparse matrices. We start with sparse matrices, but then convert to
+    # dense before scaling, since this causes the matrix to become dense anyway.
+    # We have scipy.sparse, Dask with a wrapper around scipy.sparse, and Dask with pydata sparse.
+    # The latter doesn't currently work.
+
+    # time_sparse: 30s to create matrix, 10s to run recipe
+    # time_sparse_dask: 29s to create matrix, 3.3s to run recipe
+    # So we see that Dask again can take advantage of cores.
+
     time_sparse()
+    time_sparse_dask()
+    #time_pydata_sparse_dask()
 
