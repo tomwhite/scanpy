e64a2714d7f4d3b90a694e3c8d3faeebbf0c57db
Tom White <tom.e.white@gmail.com>
2019-10-09 16:33:18 +0100

    fix

--- scanpy/tests/test_cu_sparse_dask_array.py
+++ scanpy/tests/test_cu_sparse_dask_array.py
@@ -177,7 +177,7 @@ class TestCuSparseDaskArray:
     def test_sum(self, x, xd):
         totald = np.sum(xd)
         total = np.sum(x)
-        assert totald == pytest.approx(total)
+        assert np.asarray(totald) == pytest.approx(total)
 
     def test_sum_cols(self, x, xd):
         xd = np.sum(xd, axis=0)
@@ -192,7 +192,7 @@ class TestCuSparseDaskArray:
     def test_mean(self, x, xd):
         meand = np.mean(xd)
         mean = np.mean(x)
-        assert meand == pytest.approx(mean)
+        assert np.asarray(meand) == pytest.approx(mean)
 
     def test_mean_cols(self, x, xd):
         xd = np.mean(xd, axis=0)
